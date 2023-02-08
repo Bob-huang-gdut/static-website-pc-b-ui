f2a1eb7ac64f81d1ba55e38f76c078341355c96b
sibo_huang <18813295951@163.com>
2023-02-08 23:30:35 +0800

    feat:

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://next.umijs.org/docs/api/runtime-config#getinitialstate
-export async function getInitialState(): Promise<{ name: string }> {
-  return { name: '静态网站成品' };
+export async function getInitialState(): Promise<{}> {
+  return {};
 }
 
 export const layout = () => {
